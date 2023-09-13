cf462eb54782f3cb4a8a8169cefec872870ea033
guilherme <guilherme.salviano@hotmail.com>
2023-09-13 16:06:19 -0300

    home and search button are working correctly but just the style part

--- src/components/leftMenu/homeAndSearch/index.jsx
+++ src/components/leftMenu/homeAndSearch/index.jsx
@@ -1,19 +1,30 @@
 import styles from "./homeAndSearch.module.scss"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
+
 
 export default function HomeAndSearch() {
+	const [home, setHome] = useState(true)
+	const [search, setSearch] = useState(false)
+
+
   return (
     <div className={styles.container}>
-        <button className={styles.container__button}>
+        <button 
+			className={`${styles.container__button} ${home? styles["container__button-active"] : ""}`}
+			onClick={() =>{setHome(true); setSearch(false)}}
+		>
         <FontAwesomeIcon className={styles.container__icon} icon={faHouse} />
             Home
         </button>
-        <button className={styles.container__button}>
+        <button 
+			className={`${styles.container__button} ${search? styles["container__button-active"] : ""}` }
+			onClick={() =>{setHome(false); setSearch(true)}}
+		>
             <FontAwesomeIcon className={styles.container__icon} icon={faMagnifyingGlass} />
             Search
         </button>
-        
     </div>
   )
 }
