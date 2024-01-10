89d251b4ed698df6a880295ca970096fea6ed88b
guilhermesalviano <guilherme.salviano@hotmail.com>
2024-01-09 21:04:36 -0300

    footer applying data from parent props

--- src/components/myFooter/index.jsx
+++ src/components/myFooter/index.jsx
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./myFooter.module.scss";
 
-export default function MyFooter(song) {
-	
+
+export default function MyFooter({selectedSong}) {
+	let [name, setName] = useState("")
+	let [band, setBand] = useState("")
+	let [albumImage, setAlbumImage] = useState('')
+
+	useEffect(() => {
+		if(selectedSong){
+			setName(selectedSong.track.name)
+			setBand(selectedSong.track.artists[0].name)
+			setAlbumImage(selectedSong.track.album.images[0].url)
+		}
+	}, [selectedSong])
+
 	return (
 		<footer className={styles.container}>
 			<div className={styles.container__folder}>
-			<img src={""} alt="" />
+			<img src={albumImage} alt="foto do álbum da música selecionada" />
 			<div>
-				<p>{''}</p>
-				<p></p>
+				<p>{name}</p>
+				<p>{band}</p>
 			</div>
 			</div>
 		</footer>
 	);
-	
 	
 }
