14fc68e852ec416d85c80fd4349e3b69d4b6a546
guilhermesalviano <guilherme.salviano@hotmail.com>
2024-01-16 21:15:47 -0300

    working on translate

--- src/components/myFooter/index.jsx
+++ src/components/myFooter/index.jsx
@@ -13,16 +13,20 @@ export default function MyFooter({selectedSong}) {
 			setBand(selectedSong.track.artists[0].name)
 			setAlbumImage(selectedSong.track.album.images[0].url)
 		}
+
+		if(name.length > 45){
+			console.log('acima de 45 caracteres')
+		}
 	}, [selectedSong])
 
 	return (
 		<footer className={styles.container}>
 			<div className={styles.container__folder}>
-			<img src={albumImage} alt="foto do álbum da música selecionada" />
-			<div>
-				<p>{name}</p>
-				<p>{band}</p>
-			</div>
+				<img src={albumImage} alt="foto do álbum da música selecionada" />
+				<div className={styles.container__folder__band}>
+					<p>{name}</p>
+					<p>{band}</p>
+				</div>
 			</div>
 		</footer>
 	);
